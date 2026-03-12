334e03a3ad5d1d5069c5a73b4dde04599170132f
Thach Da <thachbovjp@gmail.com>
2026-03-12 20:06:30 +0700

    feat(8-2): Setup Redis & Kafka Connections

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -3,9 +3,16 @@ import { AuthController } from "./interfaces/controllers/auth.controller";
 import { AuthService } from "./application/services/auth.service";
 import { getLoggerModule } from "@ecommerce/core";
 import { DatabaseModule } from "./infrastructure/database/database.module";
+import { RedisModule } from "./infrastructure/redis/redis.module";
+import { KafkaProducerModule } from "./infrastructure/kafka/kafka-producer.module";
 
 @Module({
-  imports: [getLoggerModule(), DatabaseModule],
+  imports: [
+    getLoggerModule(),
+    DatabaseModule,
+    RedisModule,
+    KafkaProducerModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ apps/auth-service/src/infrastructure/kafka/kafka-producer.module.ts
@@ -0,0 +1,30 @@
+import { Module, Global } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { Partitioners } from "kafkajs";
+
+export const KAFKA_SERVICE = "KAFKA_SERVICE";
+
+@Global()
+@Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: KAFKA_SERVICE,
+        useFactory: () => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: "auth-service",
+              brokers: [process.env.KAFKA_BROKERS || "localhost:9092"],
+            },
+            producer: {
+              createPartitioner: Partitioners.LegacyPartitioner, // Prevent breaking changes on NestJS/KafkaJS
+            },
+          },
+        }),
+      },
+    ]),
+  ],
+  exports: [ClientsModule],
+})
+export class KafkaProducerModule {}
